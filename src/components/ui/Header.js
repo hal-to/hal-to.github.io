e03bfce9da74d1727b4cf7bc53dc22980a4170fe
sshim <mechurak@gmail.com>
2021-01-13 17:51:53 +0900

    Add loadSheet function stub to Header
    - Reference: https://theoephraim.github.io/node-google-spreadsheet

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,6 +1,43 @@
 import React from "react";
 
 const Header = () => {
+  // It's all right. It's restricted by domain.
+  const K = "AIzaSyA0_136gWrkqUfV5t25yi4rKWQpEpXDl9c";
+
+  // "https://docs.google.com/spreadsheets/d/1wPR2CCImrk6Qb8jwlGy2_RfDJu5PwY18POrJ3UR1sts/edit?fbclid=IwAR3XbNYzBo6ITLkMG-C_JXc7-IfUIIFxyFb1mDIZuPjUQKD0afaXNiCJ2As#gid=1577519440";
+  const SHEET_ID = "1wPR2CCImrk6Qb8jwlGy2_RfDJu5PwY18POrJ3UR1sts"; // https://han.gl/49BQk
+  const SHEET_TITLE = "강환국 할투 채널";
+
+  const { GoogleSpreadsheet } = require("google-spreadsheet");
+  const loadSheet = async () => {
+    console.log("loadSheet()", SHEET_ID);
+
+    // Initialize the sheet - doc ID is the long id in the sheets URL
+    const doc = new GoogleSpreadsheet(SHEET_ID);
+    doc.useApiKey(K);
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+
+    const sheet = doc.sheetsByTitle[SHEET_TITLE];
+    console.log(sheet.title, sheet.rowCount);
+    const rowCount = sheet.rowCount;
+
+    await sheet.loadCells(`A2:F${rowCount}`);
+
+    for (let row = 1; row < rowCount; row++) {
+      const numCell = sheet.getCell(row, 0);
+      const categoryBigCell = sheet.getCell(row, 1);
+      const categorySmallCell = sheet.getCell(row, 2);
+      const titleCell = sheet.getCell(row, 3);
+      if (titleCell.value === null) continue;
+      const hyperlink = titleCell.hyperlink;
+      const summaryCell = sheet.getCell(row, 4);
+      const dateCell = sheet.getCell(row, 5);
+      console.log(numCell.value, titleCell.value, hyperlink, dateCell.value);
+    }
+  };
+
   return (
     <header className="header">
       <div className="title">
@@ -11,7 +48,7 @@ const Header = () => {
         <button>할투 영상</button>
         <button>추천 사이트</button>
         <button>about</button>
-        <button>갱신</button>
+        <button onClick={loadSheet}>갱신</button>
       </div>
     </header>
   );
