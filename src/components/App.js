c600608283ffe198172a7909265d465dd43659e8
sshim <mechurak@gmail.com>
2021-01-13 11:46:02 +0900

    Clean up project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">      
+      <header className="App-header">
+        할투 맵!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
