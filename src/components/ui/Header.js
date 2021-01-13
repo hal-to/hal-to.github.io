3ef63a975d3b5da1739f99199699151367763ae3
sshim <mechurak@gmail.com>
2021-01-13 16:06:48 +0900

    Add bg to header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,7 +3,16 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h2>header</h2>
+      <div className="title">
+        <h1 className="heading-1 heading-1--light">할투 맵 0.1</h1>
+        <h2 className="heading-2 heading-2--light">애, 차, 개 out!!</h2>
+      </div>
+      <div className="header-nav">
+        <button>할투 영상</button>
+        <button>추천 사이트</button>
+        <button>about</button>
+        <button>갱신</button>
+      </div>
     </header>
   );
 };
