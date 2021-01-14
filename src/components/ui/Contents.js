14ceb84f4e3f089d648a05c64f2ecbb8bf81fdf4
sshim from desktop <mechurak@gmail.com>
2021-01-14 22:46:59 +0900

    Show stub contents according to selected small categories

--- src/components/ui/Contents.js
+++ src/components/ui/Contents.js
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from "react";
 
-const Contents = ({selectedSmCat, videos}) => {
+const Contents = ({ selectedSmCat, videos }) => {
+  const [filteredVideos, setFilteredVideos] = useState([]);
+
   useEffect(() => {
     console.log(selectedSmCat);
-    console.log(videos.length);
+    const tempVideos = videos.filter((video) => {
+      for (let i = 0; i < video.cat_sm_list.length; i++) {
+        const cat = video.cat_sm_list[i];
+        if (selectedSmCat.has(cat)) {
+          return true;
+        }
+      }
+      return false;
+    });
+    tempVideos.sort((a, b) => {
+      return b.num - a.num;
+    });
+    console.log("tempVideos", tempVideos);
+    setFilteredVideos(tempVideos);
   }, [selectedSmCat, videos]);
 
   return (
     <section className="contents">
       <h2>contents</h2>
+      {filteredVideos.map((video) => (
+        <div>
+          <a href={video.link} target="_blank">{video.title}</a>
+        </div>
+      ))}
     </section>
   );
 };
