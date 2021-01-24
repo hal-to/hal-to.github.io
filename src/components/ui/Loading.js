725df9878f4d1d8b9f7684c95a1071a0615f8857
sshim from desktop <mechurak@gmail.com>
2021-01-24 11:06:07 +0900

    Show images when loading

--- src/components/ui/Loading.js
+++ src/components/ui/Loading.js
@@ -1,10 +1,16 @@
 import React from "react";
+import icon1 from "../../img/icon1.png"
+import icon2 from "../../img/icon2.png"
+import icon3 from "../../img/icon3.png"
+
 
 const Loading = () => {
   return (
     <div id="loading" className="loading">
       <div className="loading__contents">
-        <div className="dot-falling"></div>
+        <img className="loading__icon loading__icon--1" src={icon1}/>
+        <img className="loading__icon loading__icon--2" src={icon2}/>
+        <img className="loading__icon loading__icon--3" src={icon3}/>
       </div>
     </div>
   );
